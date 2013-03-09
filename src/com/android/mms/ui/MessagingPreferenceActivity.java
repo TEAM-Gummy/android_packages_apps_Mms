55b74388713469041077fbe7499c418b4f226a4f
Bajee11 <bajee11@gmail.com>
2013-03-08 23:31:25 -0500

    Breathing SMS Notification
    This will give your SMS notifications a breathing effect in the statusbar instead of being a static icon.
    
    Icon Credits: @Kroz
    
    Change-Id: I67614713fbeec8ac77e933f29b8ee6e1a5f46162
    
    Conflicts:
    	res/values/strings.xml
    	res/xml/preferences.xml
    	src/com/android/mms/ui/MessagingPreferenceActivity.java
    
    Conflicts:
    	res/values/strings.xml

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -63,6 +63,7 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String NOTIFICATION_VIBRATE     = "pref_key_vibrate";
     public static final String NOTIFICATION_VIBRATE_WHEN= "pref_key_vibrateWhen";
     public static final String NOTIFICATION_RINGTONE    = "pref_key_ringtone";
+    public static final String NOTIFICATION_BREATH      = "pref_key_sms_breath";
     public static final String AUTO_RETRIEVAL           = "pref_key_mms_auto_retrieval";
     public static final String RETRIEVAL_DURING_ROAMING = "pref_key_mms_retrieval_during_roaming";
     public static final String AUTO_DELETE              = "pref_key_auto_delete";
@@ -89,6 +90,7 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     private Preference mManageSimPref;
     private Preference mClearHistoryPref;
     private CheckBoxPreference mVibratePref;
+    private CheckBoxPreference mBreathPref;
     private CheckBoxPreference mEnableNotificationsPref;
     private CheckBoxPreference mMmsAutoRetrievialPref;
     private RingtonePreference mRingtonePref;
@@ -167,6 +169,7 @@ public class MessagingPreferenceActivity extends PreferenceActivity
         mEnableNotificationsPref = (CheckBoxPreference) findPreference(NOTIFICATION_ENABLED);
         mMmsAutoRetrievialPref = (CheckBoxPreference) findPreference(AUTO_RETRIEVAL);
         mVibratePref = (CheckBoxPreference) findPreference(NOTIFICATION_VIBRATE);
+        mBreathPref = (CheckBoxPreference) findPreference(NOTIFICATION_BREATH);
         mRingtonePref = (RingtonePreference) findPreference(NOTIFICATION_RINGTONE);
 
         setMessagePreferences();
@@ -420,4 +423,8 @@ public class MessagingPreferenceActivity extends PreferenceActivity
                 groupMmsPrefOn &&
                 !TextUtils.isEmpty(MessageUtils.getLocalNumber());
     }
+
+    public static boolean getBreathEnabled(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getBoolean(MessagingPreferenceActivity.NOTIFICATION_BREATH, false);
 }
